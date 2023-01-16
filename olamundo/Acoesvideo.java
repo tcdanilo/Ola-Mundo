15e526044187495b4940168b39d3b7be0eca7d63
tcdanilo <danilotiago3@hotmail.com>
2023-01-16 14:47:08 -0300

    adição de classes

---
+++ olamundo/Acoesvideo.java
@@ -0,0 +1,8 @@
+package olamundo;
+
+public interface Acoesvideo {
+    public void play();
+    public void pause();
+    public void like();
+        
+}
---
+++ olamundo/Pessoa.java
@@ -0,0 +1,10 @@
+package olamundo;
+
+public abstract class Pessoa{
+    public Pessoa(String nome2, int idade2, String sexo2) {
+    }
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected float experiencia;
+}
